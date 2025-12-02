2144d46996a14e321c596bcf8744ef56bcfbf009
KnoxNeon <iftey10@gmail.com>
2025-12-02 22:42:08 +0600

    add navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,49 +1,71 @@
-import React from 'react'
+
+import { Link, NavLink } from 'react-router';
+
 
 const Navbar = () => {
+  
   return (
-   <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+    <div>
+      <div className="navbar z-10 bg-sky-700 text-white font-normal border-t border-white/10 w-full text-grey shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+               <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/games">Pets & Supplies</Link>
+            </li>
+            </ul>
+          </div>
+          <NavLink className="flex lg:pl-4">
+            <img className="h-16 w-16 hover:scale-120"  src="./logo.png" alt="" />
+          </NavLink>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/games">Pets & Supplies</Link>
+            </li>
+        
+            
+          </ul>
+        </div>
+
+        <div className="navbar-end ">
+          
+            <div className='flex gap-2'>
+              <Link to="/profile"><img className='w-10 h-10 rounded-4xl border-2 border-sky-900 hover:scale-110'  alt="" /></Link>
+              <button  className="btn border-0 shadow rounded-3xl gap-1 hover:scale-110 bg-sky-900 text-white "> Logout</button>
+            </div>
+        
+        </div>
       </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2 bg-base-100 w-40 z-1">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
-  )
+  );
 }
 
 export default Navbar
