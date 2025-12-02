31ad3dfb8e37df44441f27336f24a719d362ce93
KnoxNeon <iftey10@gmail.com>
2025-12-02 21:35:11 +0600

    add packages

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../Layouts/MainLayout";
+
+
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      
+    ],
+  },
+]);
