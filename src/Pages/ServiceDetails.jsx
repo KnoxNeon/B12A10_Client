3d10d9b32d5bc2732ba0c85304ef1a9cee3a35f4
KnoxNeon <iftey10@gmail.com>
2025-12-06 23:43:44 +0600

    add take orders

--- src/Pages/ServiceDetails.jsx
+++ src/Pages/ServiceDetails.jsx
@@ -1,12 +1,15 @@
 import { useParams } from "react-router";
 import { Globe, MapPin, ChartColumnStacked, DollarSign, ShoppingCart  } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import axios from "axios";
 
 
 const ServiceDetails = () => {
     const [service, setService] = useState(null);
     const { myId } = useParams();
     const [loading, setLoading] = useState(true);
+    const {user} = useContext(AuthContext)
 
     useEffect(() => {
       fetch(`http://localhost:3000/services/${myId}`)
@@ -18,7 +21,41 @@ const ServiceDetails = () => {
         .catch((err) => console.log(err));
     }, [myId]);
 
-  
+const handleOrders = (e) =>{
+   e.preventDefault()
+
+   const form = e.target
+
+   const buyerName = form.buyerName.value
+   const buyerEmail = form.buyerEmail.value
+   const productName = form.productName.value
+   const quantity = parseInt(form.quantity.value)
+   const price = parseInt(form.price.value)
+   const address = form.address.value
+   const phone = form.phone.value
+   const note = form.note.value
+
+   const formData = {
+    productId:myId,
+     buyerName,
+     buyerEmail,
+     productName,
+     quantity,
+     price,
+     address,
+     phone,
+     note,
+     date: new Date()
+   }
+    
+   axios.post('http://localhost:3000/orders', formData)
+   .then(res =>{
+    console.log(res)
+   }).catch(err => {console.log(err)})
+
+  }
+
+
 
   if (loading) return <div>Details not found</div>;
 
@@ -36,7 +73,6 @@ const ServiceDetails = () => {
 
         <div className="absolute inset-x-0 bottom-0 p-6 sm:p-8 lg:p-12">
           <div className="max-w-7xl mx-auto flex">
-           
             <div className="flex flex-col gap-6 sm:gap-8 lg:flex-row lg:items-end">
               <div className="flex-1 space-y-4 lg:space-y-6">
                 <div>
@@ -44,42 +80,109 @@ const ServiceDetails = () => {
                     {service.name}
                   </h1>
                   <p className="text-lg sm:text-xl lg:text-2xl text-gray-300 mt-2 font-medium">
-                    Listed by <span className="text-yellow-500">{service.email}</span>
+                    Listed by{" "}
+                    <span className="text-yellow-500">{service.email}</span>
                   </p>
                 </div>
 
                 <div className="flex flex-wrap gap-3">
                   <span className="px-4 py-2 bg-sky-900 backdrop-blur rounded-full text-sm font-semibold flex items-center gap-1">
-                    <ChartColumnStacked className="w-4 h-4 text-yellow-500"/>{service.category}
+                    <ChartColumnStacked className="w-4 h-4 text-yellow-500" />
+                    {service.category}
                   </span>
                   <span className="px-4 py-2 bg-sky-900 backdrop-blur rounded-full text-sm font-semibold flex items-center ">
-                     {service.price} <DollarSign className="w-4 h-4  text-yellow-500" />
-                   
+                    {service.price}{" "}
+                    <DollarSign className="w-4 h-4  text-yellow-500" />
                   </span>
                   <span className="px-4 py-2 bg-sky-900 backdrop-blur rounded-full text-sm font-semibold flex items-center gap-1">
                     <MapPin className="w-4 h-4 text-yellow-500" />
-                    {service.location} 
+                    {service.location}
                   </span>
                 </div>
 
-                <a
-                  href={service.location}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="inline-flex items-center gap-3 px-6 py-3 sm:px-8 sm:py-4 bg-linear-to-r from-sky-700 to-sky-400 hover:from-sky-800 hover:to-sky-900 rounded-xl font-bold text-lg shadow-xl transition-all transform hover:scale-105"
-                >
-                  <ShoppingCart  className="w-5 h-5 sm:w-6 sm:h-6 text-yellow-500" />
-                  Order Now
+                <a onClick={() => document.getElementById("my_modal_4").showModal()}
+                  className="inline-flex items-center gap-3 px-6 py-3 sm:px-8 sm:py-4 bg-linear-to-r from-sky-700 to-sky-400 hover:from-sky-800 hover:to-sky-900 rounded-xl font-bold text-lg shadow-xl transition-all transform hover:scale-105 cursor-pointer">
+                  <ShoppingCart className="w-5 h-5 sm:w-6 sm:h-6 text-yellow-500" />Order Now
                 </a>
+
+                <dialog id="my_modal_4" className="modal">
+                  <div className="modal-box bg-gray-900 text-white border border-sky-700/40 shadow-2xl rounded-2xl p-8 max-w-3xl">
+                    <h3 className="text-2xl font-bold mb-4 text-sky-400">
+                      Place Your Order
+                    </h3>
+                    <p className="text-gray-300 mb-6">
+                      Fill in your information below to complete the order.
+                    </p>
+
+                    <form onSubmit={handleOrders} method="dialog" className="space-y-4">
+
+                      <div className="flex flex-col">
+                        <label className="text-sm font-semibold">Buyer Name</label>
+                        <input type="text" defaultValue={user.displayName} readOnly name="buyerName"
+                          className="bg-gray-800 border border-gray-700 rounded-lg px-3 py-2 text-white"/>
+                      </div>
+
+                      <div className="flex flex-col">
+                        <label className="text-sm font-semibold">Buyer Email</label>
+                        <input type="email" defaultValue={user.email} readOnly name="buyerEmail"
+                          className="bg-gray-800 border border-gray-700 rounded-lg px-3 py-2 text-white"/>
+                      </div>
+
+                      <div className="flex flex-col">
+                        <label className="text-sm font-semibold">Product Name</label>
+                        <input required defaultValue={service.name} readOnly="text" name="productName"
+                          className="bg-gray-800 border border-gray-700 rounded-lg px-3 py-2 text-white"/>
+                      </div>
+
+                      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                        <div className="flex flex-col">
+                          <label className="text-sm font-semibold"> Quantity</label>
+                          <input required type="number" name="quantity"
+                            className="bg-gray-800 border border-gray-700 rounded-lg px-3 py-2 text-white"/>
+                        </div>
+
+                        <div className="flex flex-col">
+                          <label className="text-sm font-semibold">Price($)</label>
+                          <input defaultValue={service.price} type="number" name="price" readOnly
+                            className="bg-gray-800 border border-gray-700 rounded-lg px-3 py-2 text-white"/>
+                        </div>
+                      </div>
+
+                      <div className="flex flex-col">
+                        <label className="text-sm font-semibold">Address</label>
+                        <input required type="text" name="address"
+                          className="bg-gray-800 border border-gray-700 rounded-lg px-3 py-2 text-white"/>
+                      </div>
+
+                      <div className="flex flex-col">
+                        <label className="text-sm font-semibold">Phone</label>
+                        <input required type="text" name="phone"
+                          className="bg-gray-800 border border-gray-700 rounded-lg px-3 py-2 text-white"/>
+                      </div>
+
+                      <div className="flex flex-col">
+                        <label className="text-sm font-semibold">Additional Note</label>
+                        <input type="text" name="note"
+                          className="bg-gray-800 border border-gray-700 rounded-lg px-3 py-2 text-white"/>
+                      </div>
+
+                      {/* Modal Buttons */}
+                      <div className="flex justify-end gap-3 pt-4">
+                        <button onClick={() => document.getElementById("my_modal_4").close()} className="btn px-5 py-2 rounded-lg bg-gray-700 hover:bg-gray-600 transition font-semibold">Close</button>
+                        <button type="submit" className="btn px-5 py-2 rounded-lg bg-sky-600 hover:bg-sky-700 text-white font-semibold shadow-lg">Confirm Order</button>
+                      </div>
+
+                    </form>
+                  </div>
+                               
+                </dialog>
               </div>
             </div>
-             <div className="w-full lg:w-auto flex justify-center lg:justify-end mt-8 lg:mt-0">
-      <img
-        src={service.image || '/fallback.jpg'}
-        alt={service.name}
-        className="w-full max-w-md lg:max-w-lg xl:max-w-xl h-80 lg:h-96 xl:h-[500px] object-cover rounded-2xl shadow-2xl border-4 border-sky-700/50"
-      />
-    </div>
+            
+            <div className="w-full lg:w-auto flex justify-center lg:justify-end mt-8 lg:mt-0">
+              <img src={service?.image} alt={service.name} className="w-full max-w-md lg:max-w-lg xl:max-w-xl h-80 lg:h-96 xl:h-[500px] object-cover rounded-2xl shadow-2xl border-4 border-sky-700/50"/>
+            </div>
+
           </div>
         </div>
       </div>
